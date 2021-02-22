7cc3e14c2124ea7f83ae2f0854a10fe2cfe557ed
Emilio Panizza <emilio.panizza@gmail.com>
2021-02-22 16:33:28 -0300

    github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxFsHfEkbfB-tYRmqj8FOJQBoPYNxpxWI",
+    authDomain: "fla-sis.firebaseapp.com",
+    projectId: "fla-sis",
+    storageBucket: "fla-sis.appspot.com",
+    messagingSenderId: "636133214340",
+    appId: "1:636133214340:web:56a556032028d29979dad1"
+  }
 };
